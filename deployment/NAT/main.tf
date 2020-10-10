ea573761ed937071a46bb5b80c62acb457386b17
EC2 Default User <ec2-user@ip-172-31-45-107.eu-west-1.compute.internal>
2020-10-10 19:16:27 +0000

    resolved merge conflicts

--- deployment/NAT/main.tf
+++ deployment/NAT/main.tf
@@ -1,10 +1,7 @@
 resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gw_eip.id
   subnet_id     = var.public_subnet_id
-<<<<<<< HEAD
-=======
   depends_on = [var.internet_gateway_id]
->>>>>>> d7c0fc58b50f19706ce06e1d63377b978282d7b9
 }
 
 resource "aws_eip" "nat_gw_eip" {
--- deployment/main.tf
+++ deployment/main.tf
@@ -4,7 +4,6 @@ provider "aws" {
   shared_credentials_file = "/home/ec2-user/.aws/credentials"
 }
 
-<<<<<<< HEAD
 #module "vpc" {
 #  source            = "./VPC"
 #  availability_zone = var.availability_zone
@@ -13,40 +12,17 @@ provider "aws" {
 module "internet_gateway" {
   source = "./IGW"
   vpc_id = var.vpc_id
-=======
-data "aws_availability_zones" "available_zones" {
-  state = "available"
-}
-
-module "vpc" {
-  source            = "./VPC"
-  availability_zone = data.aws_availability_zones.available_zones.names[0]
-
-}
-module "internet_gateway" {
-  source = "./IGW"
-  vpc_id = module.vpc.vpc_id
->>>>>>> d7c0fc58b50f19706ce06e1d63377b978282d7b9
 }
 
 module "security_groups" {
   source = "./SG"
-<<<<<<< HEAD
   vpc_id = var.vpc_id
-=======
-  vpc_id = module.vpc.vpc_id
->>>>>>> d7c0fc58b50f19706ce06e1d63377b978282d7b9
 }
 
 module "subnets" {
   source            = "./SUBNETS"
-<<<<<<< HEAD
   vpc_id            = var.vpc_id
   availability_zone = var.availability_zone
-=======
-  vpc_id            = module.vpc.vpc_id
-  availability_zone = data.aws_availability_zones.available_zones.names[0]
->>>>>>> d7c0fc58b50f19706ce06e1d63377b978282d7b9
   security_group_id = module.security_groups.security_group_id
   private_nated_rt_association_id = module.route_table.private_nated_rt_association_id
 }
@@ -54,9 +30,6 @@ module "subnets" {
 module "nat" {
   source           = "./NAT"
   public_subnet_id = module.subnets.public_subnet_id
-<<<<<<< HEAD
-
-=======
   internet_gateway_id = module.internet_gateway.internet_gateway_id
 
 }
@@ -68,7 +41,6 @@ module "route_table" {
   public_subnet_id    = module.subnets.public_subnet_id
   private_subnet_id   = module.subnets.private_subnet_id
   nat_gateway_id      = module.nat.nat_gateway_id
->>>>>>> d7c0fc58b50f19706ce06e1d63377b978282d7b9
 }
 
 module "route_table" {
